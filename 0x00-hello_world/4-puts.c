fee6daa225f591f7b936bd77bbf79c452c5655cc
Eman Saad <Eman@example.com>
2023-08-10 19:37:41 -0700

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/*
+/**
  * this main -it to print output using puts
- * */
+ * return alwayse(0) 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
