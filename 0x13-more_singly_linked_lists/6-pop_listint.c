8e751fc8e10c03b6347ed6ab43610f7e2b7096a8
Ema-Saad <Eman@example.com>
2023-09-26 17:09:00 -0700

    test

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * pop_listint - pop head of list
+ * @head: pointer
+ * Return: 0 if list is empty,else node's data
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *trav;
+	int value;
+
+	if (*head == NULL || head == NULL)
+		return (0);
+	value = (*head)->n;
+
+	trav = (*head)->next;
+
+	free(head);
+	*head = trav;
+
+	return (value);
+}
