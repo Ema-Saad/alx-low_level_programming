5f73b73cfcb07e60a1dc0d8e7886ad0f24d6847b
Ema-Saad <Eman@example.com>
2023-10-02 15:56:46 -0700

    i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = write(fd, text_content, l);
+	wr = write(file, text_content, l);
 
 	if (file == -1 || wr == -1)
 		return (-1);
